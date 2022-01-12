e483b1d246bd1ef406b9ddda12bb81d06a72c28e
AdrianCardin <acardinlozano@gmail.com>
2022-01-12 11:05:13 +0100

    corregido productoCrear

--- admin/ProductoActualizar.php
+++ admin/ProductoActualizar.php
@@ -6,7 +6,7 @@ $producto = new Producto($_REQUEST["id"], $_REQUEST["denominacion"], $_REQUEST["
 
 $producto = DAO::productoActualizar($producto);
 
-$traza = new Traza($_SESSION["id"], "ProductoActualizar", "Ha actualizado producto ", $_REQUEST["id"], date("F j, Y, g:i a"));
+$traza = new Traza($_SESSION["id"], "ProductoActualizar", "Ha actualizado producto ",$producto->getId(), date("F j, Y, g:i a"));
 // OJO ----> en el 4 campo del constructor debe ponerse el id del creado ( $categoria->getId() ) , demomento no pongo asi porq es una beta
 
 DAO::registrarAccion($traza);
