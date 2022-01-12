24a1b9e801e8bace63edde08d8dded95554aeb84
AdrianCardin <acardinlozano@gmail.com>
2022-01-12 10:14:01 +0100

    error productoCrear

--- admin/ProductoCrear.php
+++ admin/ProductoCrear.php
@@ -4,9 +4,9 @@ require_once "../_com/__RequireOnceComunes.php";
 
 $producto = DAO::productoCrear($_REQUEST["denominacion"], $_REQUEST["tipo"], $_REQUEST["precio"], $_REQUEST["stock"]);
 
-$idProducto=DAO::productoIdObtenerPorDenominacion($_REQUEST["denominacion"]);
+$pruducto=DAO::productoIdObtenerPorDenominacion($_REQUEST["denominacion"]);
 
-$traza = new Traza($_SESSION["id"], "ProductoCrear", "Ha creado un  producto ", $idProducto, date("F j, Y, g:i a"));
+$traza = new Traza($_SESSION["id"], "ProductoCrear", "Ha creado un  producto ", $producto["id"], date("F j, Y, g:i a"));
 // OJO ----> en el 4 campo del constructor debe ponerse el id del creado ( $categoria->getId() ) , demomento no pongo asi porq es una beta
 
 
